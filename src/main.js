a2383ce9ee8377e7af6d2e4cbf00bcda0714826b
S. D. Dikov <s.d.dikov@gmail.com>
2021-08-06 23:11:07 +0300

    save

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
-import { headerProfile } from './view/header-profile.js';
-import { mainNavigation } from './view/main-navigation.js';
-import { sortMenu } from './view/sort-menu.js';
-import { moreBtn } from './view/more-btn.js';
-import { films } from './view/films.js';
-import { filmsList } from './view/films-list.js';
-import { topRated } from './view/top-rated.js';
-import { mostCommented } from './view/most-commented.js';
-import { filmCard } from './view/film-card.js';
-import { footerStat } from './view/footer-stat.js';
-import { filmDetails } from './view/film-details.js';
+import { headerProfile } from "./view/header-profile.js";
+import { mainNavigation } from "./view/main-navigation.js";
+import { sortMenu } from "./view/sort-menu.js";
+import { moreBtn } from "./view/more-btn.js";
+import { films } from "./view/films.js";
+import { filmsList } from "./view/films-list.js";
+import { topRated } from "./view/top-rated.js";
+import { mostCommented } from "./view/most-commented.js";
+import { filmCard } from "./view/film-card.js";
+import { footerStat } from "./view/footer-stat.js";
+import { filmDetails } from "./view/film-details.js";
+import { generateFilm } from "./mock/film";
+
+const FILM_MOCK_COUNT = 20;
+
+const generateMocks = (count) => {
+  const filmsMockList = [];
+  for (let i = 0; i < count; i++) {
+    filmsMockList.push(generateFilm(i));
+  }
+  return filmsMockList;
+};
+
+const filmsMock = generateMocks(FILM_MOCK_COUNT);
 
 const render = (container, tamplate, place) => {
   container.insertAdjacentHTML(place, tamplate);
@@ -17,51 +30,51 @@ const render = (container, tamplate, place) => {
 const FILM_COUNT = 5;
 const EXTRA_FILM_COUNT = 2;
 
-const body = document.querySelector('body');
-const siteHeaderElement = document.querySelector('.header');
-const siteMainElement = document.querySelector('.main');
-const siteFooterElement = document.querySelector('.footer');
+const body = document.querySelector("body");
+const siteHeaderElement = document.querySelector(".header");
+const siteMainElement = document.querySelector(".main");
+const siteFooterElement = document.querySelector(".footer");
 const footerStatisticsElement = siteFooterElement.querySelector(
-  '.footer__statistics',
+  ".footer__statistics"
 );
 
-body.classList.add('hide-overflow');
+body.classList.add("hide-overflow");
 
-render(siteHeaderElement, headerProfile(), 'beforeend');
-render(siteMainElement, mainNavigation(), 'beforeend');
-render(siteMainElement, sortMenu(), 'beforeend');
-render(siteMainElement, films(), 'beforeend');
-render(footerStatisticsElement, footerStat(), 'beforeend');
-render(siteMainElement, filmDetails(), 'beforeend');
+render(siteHeaderElement, headerProfile(), "beforeend");
+render(siteMainElement, mainNavigation(), "beforeend");
+render(siteMainElement, sortMenu(), "beforeend");
+render(siteMainElement, films(), "beforeend");
+render(footerStatisticsElement, footerStat(), "beforeend");
+render(siteMainElement, filmDetails(filmsMock[0]), "beforeend");
 
-const mainFilmsElement = siteMainElement.querySelector('.films');
+const mainFilmsElement = siteMainElement.querySelector(".films");
 
-render(mainFilmsElement, filmsList(), 'beforeend');
-render(mainFilmsElement, topRated(), 'beforeend');
-render(mainFilmsElement, mostCommented(), 'beforeend');
+render(mainFilmsElement, filmsList(), "beforeend");
+render(mainFilmsElement, topRated(), "beforeend");
+render(mainFilmsElement, mostCommented(), "beforeend");
 
-const filmsListElement = mainFilmsElement.querySelector('.films-list');
+const filmsListElement = mainFilmsElement.querySelector(".films-list");
 const filmsListContainerElement = mainFilmsElement.querySelector(
-  '.films-list__container--main-list',
+  ".films-list__container--main-list"
 );
 
 for (let i = 0; i < FILM_COUNT; i++) {
-  render(filmsListContainerElement, filmCard(), 'beforeend');
+  render(filmsListContainerElement, filmCard(filmsMock[i]), "beforeend");
 }
-render(filmsListElement, moreBtn(), 'beforeend');
+render(filmsListElement, moreBtn(), "beforeend");
 
 const topRatedElement = mainFilmsElement.querySelector(
-  '.films-list__container--top-rated',
+  ".films-list__container--top-rated"
 );
 
 for (let i = 0; i < EXTRA_FILM_COUNT; i++) {
-  render(topRatedElement, filmCard(), 'beforeend');
+  render(topRatedElement, filmCard(), "beforeend");
 }
 
 const mostCommentedElement = mainFilmsElement.querySelector(
-  '.films-list__container--most-commented',
+  ".films-list__container--most-commented"
 );
 
 for (let i = 0; i < EXTRA_FILM_COUNT; i++) {
-  render(mostCommentedElement, filmCard(), 'beforeend');
+  render(mostCommentedElement, filmCard(), "beforeend");
 }
--- src/view/film-card.js
+++ src/view/film-card.js
@@ -1,18 +1,35 @@
-export const filmCard = () =>
-  `<article class="film-card">
-    <h3 class="film-card__title">Santa Claus Conquers the Martians</h3>
-    <p class="film-card__rating">2.3</p>
+import dayjs from "dayjs";
+import { getHoursMins } from "../utils.js";
+
+export const filmCard = (film) => {
+  const {
+    comments,
+    title,
+    totalRating,
+    poster,
+    date,
+    runtime,
+    genre,
+    description,
+  } = film;
+
+  const hoursMinsRuntime = getHoursMins(runtime);
+
+  return `<article class="film-card">
+    <h3 class="film-card__title">${title}</h3>
+    <p class="film-card__rating">${totalRating}</p>
     <p class="film-card__info">
-      <span class="film-card__year">1964</span>
-      <span class="film-card__duration">1h 21m</span>
-      <span class="film-card__genre">Comedy</span>
+      <span class="film-card__year">${dayjs(date).format("YYYY")}</span>
+      <span class="film-card__duration">${hoursMinsRuntime}</span>
+      <span class="film-card__genre">${genre}</span>
     </p>
-    <img src="./images/posters/santa-claus-conquers-the-martians.jpg" alt="" class="film-card__poster">
-    <p class="film-card__description">The Martians Momar ("Mom Martian") and Kimar ("King Martian") are worried that their children Girmar ("Girl Martian") and Bomar ("Boy Marti…</p>
-    <a class="film-card__comments">465 comments</a>
+    <img src="./${poster}" alt="" class="film-card__poster">
+    <p class="film-card__description">${description}</p>
+    <a class="film-card__comments">${comments.length} comments</a>
     <div class="film-card__controls">
       <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
       <button class="film-card__controls-item film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
       <button class="film-card__controls-item film-card__controls-item--favorite film-card__controls-item--active" type="button">Mark as favorite</button>
     </div>
   </article>`;
+};
